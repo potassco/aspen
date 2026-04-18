71869001a90648981fd1d31b765f11d5402d6e65
Amade Nemes <nemesamade@gmail.com>
2026-04-18 11:50:13 +0200

    Add some more test cases and fix bugs with local/global vars and neg

--- tests/test_aspcore2.py
+++ tests/test_aspcore2.py
@@ -16,3 +16,83 @@ class TestMetaAsp(AspenTestCase):
             meta_files=[encoding_dir / "aspcore2_list_syntax.lp"],
             expected_sources=["p(cons(1, cons(2, cons(3, nil))))."],
         )
+
+    def test_aspcore2_safety_safe1(self) -> None:
+        """Test detection of safety on safe rule."""
+        source_str = "p(X,Y) :- q(X), #sum{S,X: r(T,X), S = (2 * T) - X} = Y."
+        self.assert_transform_isomorphic(
+            language=aspcore2_lang,
+            sources=[source_str],
+            meta_files=[encoding_dir / "aspcore2_safety.lp"],
+            expected_sources=[source_str],
+        )
+
+    def test_aspcore2_safety_safe2(self) -> None:
+        """Test detection of safety on safe rule. The variable S is
+        safe, as both X and T are global, and as such the equation
+        defining S in the aggregate element is bound."""
+        source_str = "p(X,Y) :- q(X), r(T), #sum{S,X: S = (2 * T) - X} = Y."
+        self.assert_transform_isomorphic(
+            language=aspcore2_lang,
+            sources=[source_str],
+            meta_files=[encoding_dir / "aspcore2_safety.lp"],
+            expected_sources=[source_str],
+        )
+
+    def test_aspcore2_safety_safe3(self) -> None:
+        """Test detection of safety on safe rule. The variable S is
+        safe, as both X and T are global, and as such the equation
+        defining S in the aggregate element is bound."""
+        source_str = "p(X, Y, Z) :- q(X), Y = X + 2, Z = X + Y."
+        self.assert_transform_isomorphic(
+            language=aspcore2_lang,
+            sources=[source_str],
+            meta_files=[encoding_dir / "aspcore2_safety.lp"],
+            expected_sources=[source_str],
+        )
+
+    def test_aspcore2_safety_unsafe1(self) -> None:
+        """Test detection of safety on unsafe rule. The rule is unsafe
+        as the equation in the aggregate element is not of the correct
+        form to bind S."""
+        source_str = "p(X,Y) :- q(X), #sum{S,X: r(T,X), S + X = 2 * T} = Y."
+
+        self.assert_transform_raises(
+            message_regex=(
+                r"s\(0\):1:34-35: Variable 'S' is unsafe.\n"
+                r"s\(0\):1:21-22: Variable 'S' is unsafe."
+            ),
+            language=aspcore2_lang,
+            sources=[source_str],
+            meta_files=[encoding_dir / "aspcore2_safety.lp"],
+        )
+
+    def test_aspcore2_safety_unsafe2(self) -> None:
+        """Test detection of safety on unsafe rule. The rule is unsafe
+        due to default negation of the only atom in the body."""
+        source_str = "p(X) :- not r(X)."
+
+        self.assert_transform_raises(
+            message_regex=(
+                r"s\(0\):1:14-15: Variable 'X' is unsafe.\n"
+                r"s\(0\):1:2-3: Variable 'X' is unsafe."
+            ),
+            language=aspcore2_lang,
+            sources=[source_str],
+            meta_files=[encoding_dir / "aspcore2_safety.lp"],
+        )
+
+    def test_aspcore2_safety_unsafe3(self) -> None:
+        """Test detection of safety on unsafe rule. The rule is unsafe
+        due to default negation of the equality atom."""
+        source_str = "p(X,Y) :- q(X), not #sum{ S: r(S) } = Y."
+
+        self.assert_transform_raises(
+            message_regex=(
+                r"s\(0\):1:38-39: Variable 'Y' is unsafe.\n"
+                r"s\(0\):1:4-5: Variable 'Y' is unsafe."
+            ),
+            language=aspcore2_lang,
+            sources=[source_str],
+            meta_files=[encoding_dir / "aspcore2_safety.lp"],
+        )
