f1e589f16314ffea23a46f6476dd4de4fc1e4a76
Amade Nemes <nemesamade@gmail.com>
2026-04-29 13:01:10 +0200

    Add translation of choice rule to disjunctive rules + body aggregate.

--- tests/test_aspcore2.py
+++ tests/test_aspcore2.py
@@ -9,7 +9,7 @@ class TestMetaAsp(AspenTestCase):
     maxDiff = None
 
     def test_aspcore2_list_syntax(self) -> None:
-        """Test  removal of & from metasp"""
+        """Test translation of list function to cons list on nested tuples."""
         self.assert_transform_isomorphic(
             language=aspcore2_lang,
             sources=["p(list(1,2,3))."],
@@ -59,8 +59,8 @@ class TestMetaAsp(AspenTestCase):
 
         self.assert_transform_raises(
             message_regex=(
-                r"s\(0\):1:21-22: Variable 'S' is unsafe.\n"
-                r"s\(0\):1:34-35: Variable 'S' is unsafe."
+                r"s\(0\):1:34-35: Variable 'S' is unsafe.\n"
+                r"s\(0\):1:21-22: Variable 'S' is unsafe."
             ),
             language=aspcore2_lang,
             sources=[source_str],
@@ -95,4 +95,20 @@ class TestMetaAsp(AspenTestCase):
             language=aspcore2_lang,
             sources=[source_str],
             meta_files=[encoding_dir / "aspcore2_safety.lp"],
+        )
+
+    def test_aspcore2_choice_sugar(self) -> None:
+        """Test translation of choice rules to body aggregate + disjunctive rules."""
+        self.assert_transform_isomorphic(
+            language=aspcore2_lang,
+            sources=["{p(a) : q(2); -p(a) : q(3)} <= 1 :- q(1)."],
+            meta_files=[encoding_dir / "aspcore2_choice_sugar.lp"],
+            expected_sources=[
+                (
+                    ":- q(1), not #count{ __choice_neg_p(a) : -p(a), q(3);"
+                    " __choice_p(a) : p(a), q(2) } <= 1.\n"
+                    "-p(a) | __choice_neg_p(a) :- q(1), q(3).\n"
+                    "p(a) | __choice_p(a) :- q(1), q(2)."
+                )
+            ],
         )
